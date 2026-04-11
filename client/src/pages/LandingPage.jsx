ae3cc0013a38e2028f7423adb808a9ff89070d74
Midhun Mohan <midhunmohan3210@gmail.com>
2026-04-11 15:53:29 +0530

    navbar issue solved

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -6,13 +6,14 @@ import TestimonialWidget from "../components/landing/TestimonialWidget";
 export default function LandingPage() {
   return (
     <main className="min-h-screen bg-white">
+      <LandingHeader />
       <div className="relative isolate overflow-hidden">
         <div className="pointer-events-none absolute inset-x-0 top-0 z-0 h-[44rem] bg-[radial-gradient(70%_58%_at_50%_0%,rgba(254,200,154,0.22),rgba(254,200,154,0.08)_45%,rgba(255,255,255,0)_78%)]" />
         <div className="relative z-10">
-          <LandingHeader />
           <HeroSection />
         </div>
       </div>
+
       <WhatsAppShowcaseSection />
       {/* Add widget here */}
       <TestimonialWidget />
